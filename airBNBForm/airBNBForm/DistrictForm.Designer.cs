6921261ffbe6fa036b8c56190d2e151fd2a44d65
deskara <55700741+deskara@users.noreply.github.com>
2019-11-30 15:50:37 +0000

    Added Labels

--- airBNBForm/airBNBForm/DistrictForm.Designer.cs
+++ airBNBForm/airBNBForm/DistrictForm.Designer.cs
@@ -37,12 +37,15 @@
             this.nHoodOutputBox = new System.Windows.Forms.ListBox();
             this.propertyOutputBox = new System.Windows.Forms.ListBox();
             this.refreshButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // editDistrictButton
             // 
             this.editDistrictButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.editDistrictButton.Location = new System.Drawing.Point(66, 80);
+            this.editDistrictButton.Location = new System.Drawing.Point(12, 80);
             this.editDistrictButton.Name = "editDistrictButton";
             this.editDistrictButton.Size = new System.Drawing.Size(135, 51);
             this.editDistrictButton.TabIndex = 2;
@@ -53,7 +56,7 @@
             // addDistrictButton
             // 
             this.addDistrictButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.addDistrictButton.Location = new System.Drawing.Point(66, 137);
+            this.addDistrictButton.Location = new System.Drawing.Point(12, 137);
             this.addDistrictButton.Name = "addDistrictButton";
             this.addDistrictButton.Size = new System.Drawing.Size(135, 51);
             this.addDistrictButton.TabIndex = 3;
@@ -64,7 +67,7 @@
             // addNHoodButton
             // 
             this.addNHoodButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.addNHoodButton.Location = new System.Drawing.Point(66, 194);
+            this.addNHoodButton.Location = new System.Drawing.Point(12, 194);
             this.addNHoodButton.Name = "addNHoodButton";
             this.addNHoodButton.Size = new System.Drawing.Size(135, 56);
             this.addNHoodButton.TabIndex = 7;
@@ -75,7 +78,7 @@
             // addPropertyButton
             // 
             this.addPropertyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.addPropertyButton.Location = new System.Drawing.Point(66, 256);
+            this.addPropertyButton.Location = new System.Drawing.Point(12, 256);
             this.addPropertyButton.Name = "addPropertyButton";
             this.addPropertyButton.Size = new System.Drawing.Size(135, 43);
             this.addPropertyButton.TabIndex = 8;
@@ -86,7 +89,7 @@
             // readFileButton
             // 
             this.readFileButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.readFileButton.Location = new System.Drawing.Point(66, 305);
+            this.readFileButton.Location = new System.Drawing.Point(12, 305);
             this.readFileButton.Name = "readFileButton";
             this.readFileButton.Size = new System.Drawing.Size(135, 54);
             this.readFileButton.TabIndex = 9;
@@ -99,7 +102,7 @@
             this.districtOutputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.districtOutputBox.FormattingEnabled = true;
             this.districtOutputBox.ItemHeight = 20;
-            this.districtOutputBox.Location = new System.Drawing.Point(333, 12);
+            this.districtOutputBox.Location = new System.Drawing.Point(333, 0);
             this.districtOutputBox.Name = "districtOutputBox";
             this.districtOutputBox.Size = new System.Drawing.Size(467, 144);
             this.districtOutputBox.TabIndex = 10;
@@ -110,7 +113,7 @@
             this.nHoodOutputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.nHoodOutputBox.FormattingEnabled = true;
             this.nHoodOutputBox.ItemHeight = 20;
-            this.nHoodOutputBox.Location = new System.Drawing.Point(333, 161);
+            this.nHoodOutputBox.Location = new System.Drawing.Point(333, 150);
             this.nHoodOutputBox.Name = "nHoodOutputBox";
             this.nHoodOutputBox.Size = new System.Drawing.Size(467, 144);
             this.nHoodOutputBox.TabIndex = 11;
@@ -121,16 +124,16 @@
             this.propertyOutputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.propertyOutputBox.FormattingEnabled = true;
             this.propertyOutputBox.ItemHeight = 16;
-            this.propertyOutputBox.Location = new System.Drawing.Point(333, 317);
+            this.propertyOutputBox.Location = new System.Drawing.Point(333, 301);
             this.propertyOutputBox.Name = "propertyOutputBox";
-            this.propertyOutputBox.Size = new System.Drawing.Size(467, 132);
+            this.propertyOutputBox.Size = new System.Drawing.Size(467, 148);
             this.propertyOutputBox.TabIndex = 12;
             this.propertyOutputBox.SelectedIndexChanged += new System.EventHandler(this.PropertyOutputBox_SelectedIndexChanged);
             // 
             // refreshButton
             // 
             this.refreshButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.refreshButton.Location = new System.Drawing.Point(66, 23);
+            this.refreshButton.Location = new System.Drawing.Point(12, 23);
             this.refreshButton.Name = "refreshButton";
             this.refreshButton.Size = new System.Drawing.Size(135, 51);
             this.refreshButton.TabIndex = 13;
@@ -138,11 +141,44 @@
             this.refreshButton.UseVisualStyleBackColor = true;
             this.refreshButton.Click += new System.EventHandler(this.RefreshButton_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(240, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 26);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Districts";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(171, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(159, 26);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Neighborhoods";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(216, 301);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 26);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Properties";
+            // 
             // DistrictForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.refreshButton);
             this.Controls.Add(this.propertyOutputBox);
             this.Controls.Add(this.nHoodOutputBox);
@@ -156,6 +192,7 @@
             this.Text = "Air BNB Property Manager";
             this.Load += new System.EventHandler(this.DistrictForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -169,6 +206,9 @@
         private System.Windows.Forms.ListBox nHoodOutputBox;
         private System.Windows.Forms.ListBox propertyOutputBox;
         private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
